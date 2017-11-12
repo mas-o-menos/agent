7420450cb822478dd433c2af8050cdb7d9b3a693
jescobar <jose.escobar@mavrix.es>
2017-11-12 20:40:01 +0100

    Safe read. Send coverage files.

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ const BASE = PATH.join(CURRENT, '..', '..', '..', 'stats');
 
 const WEBPACK = PATH.join(BASE, 'webpack.json');
 const LIGHTHOUSE = PATH.join(BASE, 'lighthouse.json');
+const COVERAGE = PATH.join(BASE, 'lcov.info');
 
-const WEBPACK_DATA = JSON.parse(FS.readFileSync(WEBPACK).toString());
-const LIGHTHOUSE_DATA = JSON.parse(FS.readFileSync(LIGHTHOUSE).toString());
+const WEBPACK_DATA = safeRead(WEBPACK);
+const LIGHTHOUSE_DATA = safeRead(LIGHTHOUSE);
+const COVERAGE_DATA = safeRead(COVERAGE);
 
 const DATA = {
   buildNumber: BUILD_NUMBER,
@@ -27,8 +29,22 @@ const DATA = {
   branch: BRANCH,
   sha1: SHA1,
   repo: REPO,
-  webpack: WEBPACK_DATA,
-  lighthouse: LIGHTHOUSE_DATA,
+  webpack: WEBPACK_DATA ? JSON.parse(WEBPACK_DATA) : null,
+  lighthouse: LIGHTHOUSE_DATA ? JSON.parse(LIGHTHOUSE_DATA) : null,
+  coverage: COVERAGE_DATA || null,
 };
 
 REQUEST({uri: URI, json: DATA, method: 'POST'});
+
+
+function safeRead(path) {
+    let data;
+    try {
+        data = FS.readFileSync(path);
+    } catch (e) {
+        console.log('Stats file not found', path);
+        data = null;
+    }
+
+    return data;
+}
